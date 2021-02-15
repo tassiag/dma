1305febc2a23d0b365fea42a15f9c66da2fbd229
Tassia <tassigomes@gmail.com>
2021-02-15 20:33:08 +0000

    add changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+//sidebar function
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -9,6 +11,7 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
+// random middle photo
 window.onload = choosePic;
 
 var images = new Array("./img/img2.jpg", "./img/img3.jpg", "./img/principal.jpg");
@@ -17,3 +20,5 @@ function choosePic() {
     var randomNum = Math.floor(Math.random() * images.length);
     document.getElementById("random-image").src = images[randomNum];
 }
+
+
