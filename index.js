64b4f098ce9fb4469439b84f84008889f310f92a
tassiag <tassigomes@gmail.com>
2021-02-27 16:55:22 +0000

    remove random photo

--- index.js
+++ index.js
@@ -10,14 +10,3 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-
-// random middle photo
-window.onload = choosePic;
-
-var images = new Array("./img/center1.jpg", "./img/center2.jpg", "./img/center3.jpg", "./img/center4.jpg");
-
-function choosePic() {
-    var randomNum = Math.floor(Math.random() * images.length);
-    document.getElementById("random-image").src = images[randomNum];
-}
-
