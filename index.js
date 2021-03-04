be242cd432f39a17e904764ab95610d437c442a5
tassiag <tassigomes@gmail.com>
2021-03-04 17:13:07 +0000

    edit on hamburguer element

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
+
+
+
 //sidebar function
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
 }
 
 function closeNav() {
